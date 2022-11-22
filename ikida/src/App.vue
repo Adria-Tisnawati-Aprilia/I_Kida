1cd7d3dbc53419be834110364f659e4781b015e1
Adria-Tisnawati-Aprilia <adriatisnawatiaprilia@gmail.com>
2022-11-22 12:21:01 +0700

    membuat kerangka

---
+++ ikida/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>My Music</h1>
+    </header>
+    <main>
+      <section class="player">
+        <h2 class="song-title"> Test </h2>
+      </section>
+    </main>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: sans-serif;
+}
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 15px;
+  background: #212121;
+  color: #fff;
+}
+</style>
